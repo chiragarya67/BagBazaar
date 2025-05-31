0a3c995fd6d398f2f1b7c3786ceef574c092ea42
chiragarya67 <chiragarya5678@gmail.com>
2025-05-31 10:44:03 +0530

    31st may - added seprartion of concerns in models and created admin model

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require ('cookie-parser');
+const path = require('path');
+
+const AdminRouter = require("./routes/AdminRouter")
+const usersRouter = require("./routes/usersRouter")
+const productRouter = require("./routes/productRouter")
+
+
+const db = require ("./config/mongoose-connection")
+
+app.use(express.json());    
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());    
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+app.use("/Admin", AdminRouter);
+app.use("/users", usersRouter);
+app.use("/products", productRouter);
+
+
+app.listen(3000, () => {    
+    console.log('Server is running on http://localhost:3000');
+});
